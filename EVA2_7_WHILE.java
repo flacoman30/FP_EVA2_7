2a832d43308ecd22f61a8f632dc0ff41312e5f0b
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:32:42 -0600

    Add files via upload

---
+++ EVA2_7_WHILE.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_while;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA2_7_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //IMPRIMIR EL NUMERO AL 1 AL 10
+        int val = 1;
+        while(val<=10){//while---->mientras(la condicion sea verda)
+            System.out.print(val + "-");
+           //val++;
+        
+        }
+        val = 1;
+        do {
+            
+        }
+        while();
+    }
+    
+}
